11abbd6591a42743530c2992e7b8fc456bb6ecc0
loiPH <loiph.vncoder@users.noreply.github.com>
2018-11-01 11:21:42 +0700

    update

--- local/resources/views/frontend/blog-details/p_2.blade.php
+++ local/resources/views/frontend/blog-details/p_2.blade.php
@@ -28,15 +28,15 @@
                             <h4 class="border-bottom pb-2">TIN LIÊN QUAN </h4>
 
                             <ul>
-                                @for ($i = 0; $i < 6; $i++)
+                                @for ($i = 0; $i < 5; $i++)
                                     <li>
-                                        <div class="d-flex">
-                                           <div class="img-ttlq">
+                                        <div class="">
+                                            <div class="pr-2 float-left">
+                                           <div class="img-ttlq" style="background-image:url({{URL::asset('http://webaashi.com/TF/html/aashi-faucets/assets/img/all/wa-blog-detail.jpg')}});">
 
                                            </div>
-                                            <div>
-
                                             </div>
+                                           <h5><a href="">MAURIS ET PULVINAR CIDUNT CHECK !@# CHLA HJSLKH CHÁ JH</a></h5>
                                         </div>
                                     </li>
                                 @endfor
